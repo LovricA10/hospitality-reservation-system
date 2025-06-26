912a8b59dd0475dae8548f2c4029d7f21aa7de65
LovricA10 <lovric.andrej5@gmail.com>
2025-06-26 15:10:25 +0200

    Add inserts in sql for validator

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,4 +1,4 @@
-
+﻿
 CREATE TABLE [User] 
 (
 	[IDUser] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
@@ -71,3 +71,71 @@ CREATE TABLE Logs
 	[Level] [int] NOT NULL,
 	[Timestamp] [datetime] NOT NULL
 );
+
+INSERT INTO HospitalityType (TypeName) VALUES 
+('Beach Bar'),
+('Caffe Bar'),
+('Konoba'),
+('Pizzeria'),
+('Restoran');
+
+
+INSERT INTO [User] (Name, LastName, Email, Phone, Role, PwdHash, PwdSalt) VALUES
+('Ante', 'Antic', 'aantic@gmail.com', '0913425483', 'User', '7N+KOaRpV9MID5g6YSlX+w5CLPHiuvUMHzwpLZSPefQ=', 'qwqdpv+H4J+YhI0DXeA+qg=='),
+('Mate', 'Lovric', 'lovric@gmail.com', '0913425422', 'User', 'drL2zPsG24mTKRTdPr3fZ8gWiqinYkIsgLHVYukicNk=', 'YqwigaQp1I8ljnqsSoTiwe=='),
+('Stjepan', 'Labor', 'labor10@gmail.com', '0913425432', 'Admin', 'wJjmWCqNEfxSllkTfAYfDPa3o9ULHOCwq1duxpl4VM=', 'bgJSK1knEfcu5zm22csuQ2=='),
+('Ivan Petar', 'Solic', 'sole@gmail.com', '0913425433', 'Admin', 'bEJcfu2sTA80AW8HdEWZqLfnMUGSSL3k1aBWPhY0HzU=', 'jTXqY0kTrqQ18qRhozyui2k=='),
+('Ivan', 'Solic', 'sole23@gmail.com', '0913425433', 'Admin', 'iiSDfZ6fe8zdPkAMnUhr2OrMkDMIX75w0wN9ggnd6RE=', 'dbLkHnDuXNl9oUkLr7mSNA=='),
+('Andrej', 'Iovric', 'lovric10@gmail.com', '0913425433', 'Admin', 'bijBqh9+67SgEkWufHn1LcsCXEG2dW8gn3nJyjEjPg=', 'gJXOG3hNlUSCYTiTGly8uA=='),
+('Daniel', 'Bele', 'bele@example.com', '0913422511', 'Admin', 'agd8.sx6K4LoMNW2q3mVjmMBUcX04EE+ydh2xsHHd8=', 'zs0hscpZby6xEZBiEjMkJ0=='),
+('Leo', 'Messi', 'messi@example.com', '0913425546', 'Admin', 'M5ruEpK0brOtU0Et35xBqicGVTcZqS6Hs2Q++TVmJU=', '/POwAslHKbhSxhBxmmpd4A==');
+
+
+INSERT INTO HospitalityVenue (VenueName, Address, TypeID) VALUES
+('Batak', 'Radnička cesta 37, ZG', 1),
+('Stari Kotač', 'Aleja Bologne 18, Zagreb', 2),
+('Konoba Vinko', 'Šepurine 12, Šibenik', 2),
+('Procaffe', 'Obala Hrvatskog narodnog preporoda 6, Split', 6),
+('Mistral', 'Žnjan plaža', 7);
+
+
+
+
+INSERT INTO MenuItem (ItemName, ItemType, Price) VALUES
+('Batak piletina', 'Food', 11.75),
+('Pečena janjetina', 'Food', 15.50),
+('Šarena pljeskavica', 'Food', 9.90),
+('Graševina čaša', 'Drink', 3.20),
+('Pržene lignje', 'Food', 13.50),
+('Skradinski rižot', 'Food', 18.90),
+('Bijelo vino kuće', 'Drink', 2.90),
+('Cappuccino', 'Drink', 2.00),
+('Croissant', 'Food', 2.00),
+('Ćevapi sa sirom', 'Food', 9.50),
+('Panacota', 'Food', 3.45),
+('Koktel Mistral', 'Drink', 3.50),
+('Velika toplo', 'Drink', 2.54);
+
+INSERT INTO Reservation (NumberOfGuests, Status, ReservationDate, UserID, VenueID) VALUES
+(3, 'Confirmed', '2025-06-03 20:30:00', 3, 3),
+(5, 'Pending', '2025-07-04 16:45:00', 13, 4);
+
+
+INSERT INTO VenueMenuItem (MenuItemID, VenueID) VALUES
+(4, 3),
+(6, 4),
+(7, 4),
+(8, 4),
+(9, 5),
+(10, 5),
+(11, 5),
+(14, 7),
+(15, 7),
+(16, 4),
+(17, 7),
+(21, 9);
+
+
+INSERT INTO Logs (Message, Level, Timestamp) VALUES
+('Test log message', 1, GETDATE());
+
